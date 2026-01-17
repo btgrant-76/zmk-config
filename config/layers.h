ec44a9aa3d0b8c77b7d9b401ca61a00c44a9edc7
Brian Grant <brian@skeletonjack.net>
2026-01-17 17:33:22 -0600

    chiffre base to shared layout

---
+++ config/layers.h
@@ -0,0 +1,11 @@
+#define BASE_1_RIGHT       &kp Q  &kp W  &kp E  &kp R  &kp T
+#define BASE_1_LEFT        &kp Y  &kp U  &kp I  &kp O  &kp P
+#define BASE_2_RIGHT       HRML(A,    S,         D,         F)        &kp G
+#define BASE_2_LEFT        &kp H      HRMR3(J,   K,         L)        &qot_smi
+#define BASE_3_RIGHT       &z_esc     &kp X      &kp C      &kp V     B_MEH
+#define BASE_3_LEFT        N_MEH      &kp M      &cma_lprn &dot_rprn  &sls_ent
+#define BASE_2_THUMB_RIGHT &lt 5 TAB  &lt 2 BSPC
+#define BASE_2_THUMB_LEFT  &lt 3 SPACE &lt 5 DEL
+
+//#define BASE_3_THUMB_RIGHT
+//#define BASE_3_THUMB_LEFT
