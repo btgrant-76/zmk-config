61dc1478a45da26d8f0086c12f15b005c35c7fa0
Brian Grant <brian@skeletonjack.net>
2026-01-17 22:15:02 -0600

    STM Chiffre on central keymap

--- config/layers.h
+++ config/layers.h
@@ -7,5 +7,80 @@
 #define BASE_2_THUMB_RIGHT &lt 5 TAB  &lt 2 BSPC
 #define BASE_2_THUMB_LEFT  &lt 3 SPACE &lt 5 DEL
 
-//#define BASE_3_THUMB_RIGHT
-//#define BASE_3_THUMB_LEFT
+#define MED_1_RIGHT        &none      &none        &none        &none      &none
+#define MED_1_LEFT         &none    &none      &none      &none      &none
+#define MED_2_RIGHT        &kp C_EV &kp C_VOL_DN &kp C_VOL_UP &kp C_NEXT &none
+#define MED_2_LEFT         &none    &none      &none      &none      &none
+#define MED_3_RIGHT        &none    &none        &none        &none      &none
+#define MED_3_LEFT         &none    &none      &none      &none      &none
+
+#define MED_2_THUMB_RIGHT  &kp C_MUTE &kp C_PP
+#define MED_2_THUMB_LEFT   &none      &none
+
+//#define MED_3_THUMB_RIGHT
+//#define MED_3_THUMB_LEFT
+
+#define NUM_1_RIGHT        UNDO_      CUT_       COPY_      PASTE_      REDO_
+#define NUM_1_LEFT         &lbkt_par    &kp N7   &kp N8     &kp N9     &rbkt_par
+#define NUM_2_RIGHT        &kp LCTRL  &kp LALT   &kp LGUI   &kp LSHFT   &caps_word
+#define NUM_2_LEFT         &kp EQUAL    &kp N4   &kp N5     &kp N6     &semi_quot
+#define NUM_3_RIGHT        &kp ESC    &none      &leader    &key_repeat &none
+#define NUM_3_LEFT         &bslsh_enter &kp N1   &two_comma &three_dot &grv_slsh
+
+#define NUM_2_THUMB_RIGHT  &kp BSPC    &trans
+#define NUM_2_THUMB_LEFT   &mins_space &zero_space
+
+//#define NUM_3_THUMB_RIGHT
+//#define NUM_3_THUMB_LEFT
+
+#define NAV_1_RIGHT        TAB_LFT   BACK     FWD      TAB_RGT     &up_dir
+#define NAV_1_LEFT         REDO_     PASTE_    COPY_     CUT_      UNDO_
+#define NAV_2_RIGHT        &kp LCTRL &kp LALT &kp LGUI &kp LSHFT   &kp CAPS
+#define NAV_2_LEFT         &kp LEFT  &kp DOWN  &kp UP    &kp RIGHT &kp SEMI
+#define NAV_3_RIGHT        &none     &none    &none    &key_repeat &none
+#define NAV_3_LEFT         &kp HOME  &kp PG_DN &kp PG_UP &kp END   &kp RETURN
+
+#define NAV_2_THUMB_RIGHT  &none       &kp N
+#define NAV_2_THUMB_LEFT   &none     &none
+
+//#define NAV_3_THUMB_RIGHT
+//#define NAV_3_THUMB_LEFT
+
+#define FUN_1_RIGHT        &log_out  &none    &scrn2clp &scrn2fl  &none
+#define FUN_1_LEFT         &none    &kp F7    &kp F8    &kp F9    &kp F12
+#define FUN_2_RIGHT        &kp LCTL &kp LALT &kp LGUI  &kp LSHFT &kp C_BRI_UP
+#define FUN_2_LEFT         &none    &kp F4    &kp F5    &kp F6    &kp F11
+#define FUN_3_RIGHT        &none    &none    &none     &none     &kp C_BRI_DN
+#define FUN_3_LEFT         &none    &kp F1    &kp F2    &kp F3    &kp F10
+
+#define FUN_2_THUMB_RIGHT  &none     &mo 5
+#define FUN_2_THUMB_LEFT   &mo 6      &none
+
+//#define FUN_3_THUMB_RIGHT
+//#define FUN_3_THUMB_LEFT
+
+#define MSE_1_RIGHT        UNDO_    CUT_      COPY_     PASTE_    REDO_
+#define MSE_1_LEFT         &none         &none          &none        &none           &none
+#define MSE_2_RIGHT        &mkp MB &mkp MB3  &mkp MB1  &mkp MB2  &none
+#define MSE_2_LEFT         &mmv MOE_LEFT &mmv MOVE_DOWN &mmv MOVE_UP &mmv MOVE_RIGHT &none
+#define MSE_3_RIGHT        &kp ESC &kp LCTRL &kp LALT  &kp LGUI  &kp LSHFT
+#define MSE_3_LEFT         &msc SCL_LEFT &msc SCRL_DOWN &msc SCRL_UP &msc SCRL_RIGHT &none
+
+#define MSE_2_THUMB_RIGHT  &none     &none
+#define MSE_2_THUMB_LEFT   &none          &none
+
+//#define MSE_3_THUMB_RIGHT
+//#define MSE_3_THUMB_LEFT
+
+#define ADD_1_RIGHT        &bt BT_CLR &bt BT_SEL 0 &bt BT_SEL 1 &bt BT_SEL 2 &none
+#define ADD_1_LEFT         REDO_     PASTE_     COPY_      CUT_       UNDO_
+#define ADD_2_RIGHT        &none     &none        &none        &none        &none
+#define ADD_2_LEFT         &none     &none      &none      &none      &none
+#define ADD_3_RIGHT        &none     &none        &none        &none        &bootloader
+#define ADD_3_LEFT         &none     &none      &none      &none      &none
+
+#define ADD_2_THUMB_RIGHT  &trans       &trans
+#define ADD_2_THUMB_LEFT   &trans       &trans
+
+//#define ADD_3_THUMB_RIGHT
+//#define ADD_3_THUMB_LEFT
